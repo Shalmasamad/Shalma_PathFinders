2db8ad2d5e9da3f420a781b47b0e19416e572e7d
Shalmasamad <Shalmasamadcse@gmail.com>
2025-11-05 21:19:07 +0530

    feat:added new file

---
+++ src/Day1/FindOddEven.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class FindOddEven {
+    public static void main(String[] args){
+        int num=8;
+        if(num%2==0) {
+            System.out.println("num is an even number");
+        }
+        else{
+            System.out.println("num is an odd number");
+        }
+    }
+}
