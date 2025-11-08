2082043c835304f2147223eb4f00aff6b9785bbe
Shalmasamad <Shalmasamadcse@gmail.com>
2025-11-08 20:48:08 +0530

    feat:add

--- src/Day3/vowelscount.java
+++ src/Day3/vowelscount.java
@@ -4,12 +4,8 @@ public class vowelscount {
         public static void main(String[] args) {
             String str = "Pathfinders";
             int count = 0;
-
-            // Loop through each character
             for (int i = 0; i < str.length(); i++) {
                 char ch = str.charAt(i);
-
-                // Check for vowels manually (both lowercase and uppercase)
                 if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
                         ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                     count++;
